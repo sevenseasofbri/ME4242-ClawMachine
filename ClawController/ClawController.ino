74efe1a7b1b4c7446f72b5cea8383aa4129a5e6b
sevenseasofbri <vishruti250102@gmail.com>
2022-11-07 11:24:57 +0800

    add rfid

--- ClawController/ClawController.ino
+++ ClawController/ClawController.ino
@@ -3,8 +3,11 @@
  * FOR ARDUINO UNO + DFR0008 Joystick Module
  */
 #include <Servo.h>
-//#include <SPI.h>
-//#include <MFRC522.h>
+#include <SPI.h>
+#include <deprecated.h>
+#include <MFRC522.h>
+#include <MFRC522Extended.h>
+#include <require_cpp11.h>
 
 // JOYSTICK MODULE - FROM DOCUMENTATION DFR0008
 #define UP_BUTTON 8 // Activate the claw
@@ -21,6 +24,8 @@ int yAxis = 0;
 boolean grab = 0;
 boolean letgo = 0;
 char state = 'L';
+boolean prevButtonState = false;
+boolean currButtonState = false;
 
 // DC MOTOR & L298N PINS
 #define ENB_L298N 3 // PWM
@@ -36,13 +41,15 @@ int motorSpeed = 0;
 
 int pulleySpeed = 0;
 
-//
-//// RFID READER
-//#define SS_PIN 10
-//#define RST_PIN 9
+// RFID READER
+#define SS_PIN 10
+#define RST_PIN 9
 
-//MFRC522 mfrc522(SS_PIN, RST_PIN);
+MFRC522 mfrc522(SS_PIN, RST_PIN);
 
+// Game State Variables
+enum game_state {PLAY, STOP};
+game_state gameState = STOP;
  
 void setup() {
   Serial.begin(115200);
@@ -54,6 +61,7 @@ void setup() {
   pinMode(JOYSTICK_BUTTON, INPUT);
   pinMode(X_AXIS_JOYSTICK, INPUT);
   pinMode(Y_AXIS_JOYSTICK, INPUT);
+  currButtonState = isButtonPressed(UP_BUTTON);
   // Init motor and L298N
   pinMode(ENB_L298N, OUTPUT);
   pinMode(IN3_L298N, OUTPUT);
@@ -65,40 +73,91 @@ void setup() {
   pinMode(IN2_L298N, OUTPUT);
   analogWrite(ENA_L298N, 0);
   // Init RFID
+  mfrc522.PCD_Init();
 }
 
 void loop() {
-  xAxis = analogRead(X_AXIS_JOYSTICK);
-  yAxis = analogRead(Y_AXIS_JOYSTICK);
-  grab = isButtonPressed(UP_BUTTON);
-  letgo = isButtonPressed(LEFT_BUTTON);
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     // Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     // Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  content.toUpperCase();
+  if (content.substring(1) == "73 C2 63 11") //change here the UID of the card/cards that you want to give access
+  {
+    //Serial.println("Enough credits - Start Game");
+    //Serial.println();
+    gameState = PLAY;
+    runGame();
+    delay(3000);
+  }
+ 
+ else   {
+    //Serial.println(" Access denied");
+    delay(3000);
+  }
+}
+
+void runGame() {
+  while (gameState == PLAY) {
+      xAxis = analogRead(X_AXIS_JOYSTICK);
+      yAxis = analogRead(Y_AXIS_JOYSTICK);
+  //  grab = isButtonPressed(UP_BUTTON);
+  //  letgo = isButtonPressed(LEFT_BUTTON);
+      prevButtonState = currButtonState;
+      currButtonState = isButtonPressed(UP_BUTTON);
 
   // SEND MESSAGE TO FLUIDIC CONTROL BOARD
-  if (grab || letgo) {
-    state = letgo ? 'L' : 'G';
-    Serial.write(state);
-    Serial.flush();
-  }
+//  if (grab || letgo) {
+//    state = letgo ? 'L' : 'G';
+//    Serial.write(state);
+//    Serial.flush();
+//  }
+      if (prevButtonState && !currButtonState) { // Toggle
+        grab = !grab;
+        state = grab ? 'G' : 'L';
+        Serial.write(state);
+        if (!grab) {
+          gameState = STOP;
+          break;
+        }
+      }
 
-  // X AXIS USED FOR LEFT-RIGHT CONTROL
-  if (xAxis < 470) { 
-    moveLeft();
-  } else if (xAxis > 550) {
-    moveRight();
-  } else {
-    stopMotor();
-  }
-  analogWrite(ENB_L298N, motorSpeed);
+      // X AXIS USED FOR LEFT-RIGHT CONTROL
+      if (xAxis < 470) { 
+        moveLeft();
+      } else if (xAxis > 550) {
+        moveRight();
+      } else {
+        stopMotor();
+      }
+      analogWrite(ENB_L298N, motorSpeed);
 
-  // Y AXIS USED FOR UP-DOWN CONTROL
-  if (yAxis < 470) {
-    moveDown();
-  } else if (yAxis > 550) {
-    moveUp();
-  } else {
-    stopPulley();
-  }
-  analogWrite(ENA_L298N, pulleySpeed);
+      // Y AXIS USED FOR UP-DOWN CONTROL
+      if (yAxis < 470) {
+        moveDown();
+      } else if (yAxis > 550) {
+        moveUp();
+      } else {
+        stopPulley();
+      }
+      analogWrite(ENA_L298N, pulleySpeed);
+   }
+   return;
 }
 
 void moveLeft() {
